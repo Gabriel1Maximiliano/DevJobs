e982008f7fc819e9a771e1b87d3a2ef05720d921
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2022-12-07 13:25:28 -0300

    It es created Vacantes model

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const mongoose = require('mongoose');
+require('./config/db'); 
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 
 const router = require('./routes/index');
 const path = require('path');
 const app = express();
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({path:'variables.env'});
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+
 
 // enable handlebars as view
 app.engine('handlebars', 
@@ -18,6 +28,16 @@ app.engine('handlebars',
 
  // static files
 app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+app.use(session({
+    secret:process.env.SECTRET,
+    key:process.env.KEY,
+    resave: false,
+    saveUninitialized:false,
+     store: new MongoStore({ mongoUrl: process.env.DATABASE }),
+
+}))
 app.use('/', router());
  
  
---
+++ models/Vacantes.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const slug = require('slug');
+
+const shortid = require('shortid');
+
+
+const vacantesSchema = new mongoose.Schema(
+    { 
+        titulo:{
+            type:String,
+            required:'El nombre de la vacante es obliatorio',
+            trim:true
+        },
+        empresa:{
+            type:String,
+            trim:true
+        },
+        ubicacion:{
+            type:String,
+            trim:true,
+            required:'La ubicación es obligatoria'
+
+        },
+        salario:{
+            type:String,
+            default:0
+        },
+        contrato:{
+            type:String
+        },
+        descripcion:{
+            type:String,
+            trim:true
+        },
+        url:{
+            type:String,
+            lowercase:true
+        },
+        skills:[String],
+        candidatos:[{
+            nombre:String,
+            email:String,
+            cv:String
+        }]
+       
+    }
+);
+
+vacantesSchema.pre('save', function(next){
+    const url = slug(this.titulo);
+    this.url = `${url}-${shortid.generate()}`
+    /**
+     * React Devloper-->react-developer-1215487
+     */
+    next();
+})
+
+module.exports = mongoose.model('Vacante',vacantesSchema)
